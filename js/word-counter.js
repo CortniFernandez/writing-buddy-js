3c87d25aaebaba48ae1baa463fbf53f06e493033
Cortni <cortni.fernandez@gmail.com>
2021-03-23 12:35:04 -0400

    JS fix for high jumps in word count

--- js/word-counter.js
+++ js/word-counter.js
@@ -15,42 +15,48 @@ $(document).ready(function() {
     $("#so-far-display").html(update);
     $('#so-far').val("");
 
+    function flowerFade() {
+      $(".flower").addClass("transparent");
+    }
+
     var percentage = update / todayGoal;
     if (percentage < 0.2) {
       $("#encouragement").html("You got this.");
     } else if (percentage >= 0.2 && percentage <= 0.3) {
       $("#encouragement").html("I believe in you.");
-      $(".flower").addClass("transparent");
+      flowerFade();
       $(".img-two").removeClass("transparent");
     } else if (percentage > 0.3 && percentage <= 0.4) {
       $("#encouragement").html("You're doing it!");
-      $(".flower").addClass("transparent");
+      flowerFade();
       $(".img-three").removeClass("transparent");
     } else if (percentage > 0.4 && percentage <= 0.5) {
       $("#encouragement").html("Look at you go!");
-      $(".flower").addClass("transparent");
+      flowerFade();
       $(".img-four").removeClass("transparent");
     } else if (percentage > 0.5 && percentage <= 0.6) {
       $("#encouragement").html("Over halfway there now.");
-      $(".flower").addClass("transparent");
+      flowerFade();
       $(".img-five").removeClass("transparent");
     } else if (percentage > 0.6 && percentage <= 0.7) {
       $("#encouragement").html("That's it, keep going!");
-      $(".flower").addClass("transparent");
+      flowerFade();
       $(".img-six").removeClass("transparent");
     } else if (percentage > 0.7 && percentage <= 0.8) {
       $("#encouragement").html("You're doing so awesome!");
-      $(".flower").addClass("transparent");
+      flowerFade();
       $(".img-seven").removeClass("transparent");
     } else if (percentage > 0.8 && percentage <= 0.9) {
       $("#encouragement").html("Don't quit now!");
-      $(".flower").addClass("transparent");
+      flowerFade();
       $(".img-eight").removeClass("transparent");
     } else if (percentage > 0.9 && percentage < 1) {
       $("#encouragement").html("Nearly there!");
+      flowerFade();
+      $(".img-eight").removeClass("transparent");
     } else if (percentage >= 1) {
       $("#encouragement").html("You did it!!! Thank you for helping me grow <3");
-      $(".flower").addClass("transparent");
+      flowerFade();
       $(".img-nine").removeClass("transparent");
     }
   });
